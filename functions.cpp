67f8e30778ad56f4d04b1d4ec08c015dd2295097
Chainwalker141 <jmsinjian@gmail.com>
2025-05-16 15:27:03 +0800

    initial commit

---
+++ functions.cpp
@@ -0,0 +1,5 @@
+#include "header.h"
+
+void display_ASCII(){
+    std::cout << "ASCII Test";
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "functions.cpp"
+
+int main(){
+    std::cout << "Omg cpp is working?!?!?1" << std::endl;
+    std::cout << "CSOPESY MCO1 S15 Group 4" << std::endl;
+    display_ASCII();
+    return 0;
+}
+
+
