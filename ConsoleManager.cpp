27310efdc73526aac1691d05c3e6729508eeef0d
emy-laroza <emy_louise_laroza@dlsu.edu.ph>
2025-06-28 02:36:24 +0800

    Separated running and finished in -ls

--- ConsoleManager.cpp
+++ ConsoleManager.cpp
@@ -215,25 +215,41 @@ void ConsoleManager::displayProcessSmi() {
     cout << "Cores used: " << coresUsed << endl;
     cout << "Cores available: " << coresAvailable << endl;
     cout << "-----------------------------------" << endl;
+
+    // Running Processes
     cout << "Running processes:" << endl;
     for (const auto& pair : screenMap) {
         shared_ptr<Console> screenPtr = pair.second;
 
         auto coreID = screenPtr->getProcessName();
         string coreIDstr;
+        if (screenPtr->getCurrentLine() < screenPtr->getTotalLine()) {
+            cout << "Name: " << screenPtr->getProcessName() << " | "
+                << screenPtr->getTimestamp() << " | "
+                << "Core: " << coreIDstr << " | "
+                << screenPtr->getCurrentLine() << "/"
+                << screenPtr->getTotalLine() << " | " << endl;
+        }
         /*   if (coreID == -1) {
                coreIDstr = "N/A";
            }
            else {
                coreIDstr = to_string(coreID);
            }*/
+        
+    }
 
-        cout << "Name: " << screenPtr->getProcessName() << " | "
-            << screenPtr->getTimestamp() << " | "
-            << "Core: " << coreIDstr << " | "
-            << screenPtr->getCurrentLine() << "/"
-            << screenPtr->getTotalLine() << " | " << endl;
-
+    // Finished Processes
+    cout << "\nFinished Processes:" << endl;
+    for (const auto& pair : screenMap) {
+        auto screenPtr = pair.second;
+        if (screenPtr->getCurrentLine() >= screenPtr->getTotalLine()) {
+            cout << "Name: " << screenPtr->getProcessName() << " | "
+                << screenPtr->getTimestamp() << " | "
+                << "Finished | "
+                <<screenPtr->getCurrentLine() << "/"
+                << screenPtr->getTotalLine() << " | Finished!" << endl;
+        }
     }
 }
 
