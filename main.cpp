7350c789af559555a03d0715311483f928e83a61
emy-laroza <emy_louise_laroza@dlsu.edu.ph>
2025-08-05 20:21:20 +0800

    readded screen -r logs funtion

--- main.cpp
+++ main.cpp
@@ -211,6 +211,7 @@ void Screen(std::vector<std::string> args) {
                 }
 
                 cout << "screen resumed\n";
+                ConsoleManager::getInstance()->displayProcessSmi(processName);
             }
             else {
                 cout << "Process " << processName << " not found\n";
